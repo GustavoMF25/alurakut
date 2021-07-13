40b3616405040ac889d59deb82a50ee2c80083e3
Gustavo Fernandes <gustavomarcelinofernandes@id.uff.br>
2021-07-13 18:57:15 -0300

    Inicio do AluraKut

---
+++ src/components/MainGrid/index.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const MainGrid = styled.main`
+    display: grid;
+    grid-gap: 10px;
+    padding:16px;
+    width:100%;
+    margin-left:auto;
+    margin-right: auto;
+    max-width: 500px;
+    .profileArea{
+        display: none;
+        @media(min-width: 860px){
+            display: block;
+        }
+    }
+
+    @media(min-width: 860px){
+        max-width: 1110px;
+        display: grid;
+        grid-template-areas: 
+            "profileArea welcomeArea communityArea";
+        grid-template-columns: 160px 1fr 312px;
+    }
+`;
+
+export default MainGrid;
